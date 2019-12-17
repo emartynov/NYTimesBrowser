488187d9b55f95eec02bebc13ba13cfa812c493f
Eugen Martynov <eugenemartynov@gmail.com>
2019-12-18 00:43:55 +0100

    Remove api key as function parameter

--- common-search/src/main/java/nl/bijdorpstudio/common/search/ArticleSearch.kt
+++ common-search/src/main/java/nl/bijdorpstudio/common/search/ArticleSearch.kt
@@ -1,11 +1,9 @@
 package nl.bijdorpstudio.common.search
 
 import io.reactivex.Single
-import nl.bijdorpstudio.common.article.NonEmptyString
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 internal interface ArticleSearch {
     @GET("svc/search/v2/articlesearch.json")
-    fun searchArticles(@Query("apikey") apiKey: NonEmptyString): Single<ArticleSearchResult>
+    fun searchArticles(): Single<ArticleSearchResult>
 }
