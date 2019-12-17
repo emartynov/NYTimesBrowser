458c2e28663ab4ee10700b4ba8b1e4c3cd8e6e29
Eugen Martynov <eugenemartynov@gmail.com>
2019-12-17 20:48:59 +0100

    New project generated

---
+++ app/src/main/java/nl/bijdorpstudio/nytimesbrowser/MainActivity.kt
@@ -0,0 +1,12 @@
+wpackage nl.bijdorpstudio.nytimesbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
