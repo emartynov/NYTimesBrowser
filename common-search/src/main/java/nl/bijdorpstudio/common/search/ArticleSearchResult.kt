b01c2eff4e89ca6ed8f37c266ab2fca843febd65
Eugen Martynov <eugenemartynov@gmail.com>
2019-12-18 00:44:31 +0100

    Enrich article DTO parsing

--- common-search/src/main/java/nl/bijdorpstudio/common/search/ArticleSearchResult.kt
+++ common-search/src/main/java/nl/bijdorpstudio/common/search/ArticleSearchResult.kt
@@ -2,6 +2,9 @@ package nl.bijdorpstudio.common.search
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import nl.bijdorpstudio.common.article.Article
+import nl.bijdorpstudio.common.article.NonEmptyString
+import java.time.LocalDateTime
 
 @JsonClass(generateAdapter = true)
 internal data class ArticleSearchResult(
@@ -15,5 +18,23 @@ internal data class ArticleSearchResponse(
 
 @JsonClass(generateAdapter = true)
 internal data class ArticleDTO(
-    @Json(name = "_id") val id: String
+    @Json(name = "_id") val id: String,
+    @Json(name = "pub_date") val date: String,
+    val headline: Headline,
+    val multimedia: List<Multimedia>
 )
+
+@JsonClass(generateAdapter = true)
+internal data class Headline(
+    val name: String
+)
+
+@JsonClass(generateAdapter = true)
+internal data class Multimedia(
+    val type: String,
+    val subtype: String,
+    val url: String
+)
+
+internal fun ArticleDTO.toDomain(): Article =
+    Article(NonEmptyString.of("test")!!, LocalDateTime.now())
--- common-search/src/test/kotlin/nl/bijdorpstudio/common/search/ArticleSearchResultShould.kt
+++ common-search/src/test/kotlin/nl/bijdorpstudio/common/search/ArticleSearchResultShould.kt
@@ -9,7 +9,20 @@ private val JSON = """
         "response" : {
             "docs" : [ 
                 {
-                    "_id": "nyt://recipe/4bfb1898-5152-5f6b-991b-6d30cf764b96"
+                    "_id": "nyt://recipe/4bfb1898-5152-5f6b-991b-6d30cf764b96",
+                    "pub_date": "2019-12-18T00:00:00+0000",
+                    "headline" : {
+                        "name" : "Golden Ginger Cake"
+                    },
+                    "multimedia" : [
+                        {
+                            "subtype": "slide",
+                            "type": "image",
+                            "url": "images/2019/12/18/dining/17Romanrex3/merlin_162383772_d1e931a2-acf1-402a-ba48-77c3b8b0004a-slide.jpg",
+                            "height": 400,
+                            "width": 600
+                        }
+                    ]
                 }
             ]
         }
@@ -25,7 +38,15 @@ class ArticleSearchResultShould {
         val result = adapter.fromJson(JSON) as ArticleSearchResult
 
         assertThat(result.response.documents).hasSize(1)
-        assertThat(result.response.documents[0].id)
-            .isEqualTo("nyt://recipe/4bfb1898-5152-5f6b-991b-6d30cf764b96")
+        val articleDTO = result.response.documents[0]
+        assertThat(articleDTO.id).isEqualTo("nyt://recipe/4bfb1898-5152-5f6b-991b-6d30cf764b96")
+        assertThat(articleDTO.date).isEqualTo("2019-12-18T00:00:00+0000")
+        assertThat(articleDTO.headline.name).isEqualTo("Golden Ginger Cake")
+        assertThat(articleDTO.multimedia).hasSize(1)
+        val multimedia = articleDTO.multimedia[0]
+        assertThat(multimedia.type).isEqualTo("image")
+        assertThat(multimedia.subtype).isEqualTo("slide")
+        assertThat(multimedia.url)
+            .isEqualTo("images/2019/12/18/dining/17Romanrex3/merlin_162383772_d1e931a2-acf1-402a-ba48-77c3b8b0004a-slide.jpg")
     }
 }
