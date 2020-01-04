ed584783620577dbeffd5f2e0b9a64c1cee86d60
Eugen Martynov <eugenemartynov@gmail.com>
2020-01-04 18:01:56 +0100

    Add formatter for the date

--- feature-search/src/main/java/nl/bijdorpstudio/feature/search/ArticleListFlexItem.kt
+++ feature-search/src/main/java/nl/bijdorpstudio/feature/search/ArticleListFlexItem.kt
@@ -4,16 +4,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import nl.bijdorpstudio.common.article.Article
 import nl.bijdorpstudio.feature.search.flex.FlexAdapter
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.FormatStyle
 
 // TODO: test
 class ArticleListFlexItem(private val article: Article) :
     FlexAdapter.Item(R.layout.article_list_item) {
 
-    override fun onBind(viewGroup: ViewGroup) {
-        super.onBind(viewGroup)
+    private val dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
 
+    override fun onBind(viewGroup: ViewGroup) {
         viewGroup.findViewById<TextView>(R.id.article_title).text = article.title.value
-        viewGroup.findViewById<TextView>(R.id.article_date).text = article.date.toString()
+        viewGroup.findViewById<TextView>(R.id.article_date).text =
+            article.date.format(dateTimeFormatter)
     }
 
     override fun isItemTheSame(that: FlexAdapter.Item): Boolean {
