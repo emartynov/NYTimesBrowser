df2579d750e7cd14ca4d87d4d50e88fce0ebf9e6
Eugen Martynov <eugenemartynov@gmail.com>
2020-01-05 23:02:41 +0100

    Implement draft navigation in application

--- app/src/main/java/nl/bijdorpstudio/nytimesbrowser/NYTimesApplication.kt
+++ app/src/main/java/nl/bijdorpstudio/nytimesbrowser/NYTimesApplication.kt
@@ -2,8 +2,12 @@ package nl.bijdorpstudio.nytimesbrowser
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import nl.bijdorpstudio.common.navigation.Navigator
+import nl.bijdorpstudio.common.navigation.NavigatorProvider
 
-class NYTimesApplication : Application() {
+class NYTimesApplication : Application(), NavigatorProvider {
+    override val navigator: Navigator = NavigatorImpl(this)
+
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
---
+++ app/src/main/java/nl/bijdorpstudio/nytimesbrowser/NavigatorImpl.kt
@@ -0,0 +1,10 @@
+package nl.bijdorpstudio.nytimesbrowser
+
+import android.content.Context
+import nl.bijdorpstudio.common.article.Article
+import nl.bijdorpstudio.common.navigation.Navigator
+
+class NavigatorImpl(private val context: Context) : Navigator {
+    override fun showArticleDetail(article: Article) {
+    }
+}
